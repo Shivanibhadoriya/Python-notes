10293956db87535d1da53f29db36a8c0aca20d3b
Shivanibhadoriya <shivanibhadouriya2003@gmail.com>
2024-07-25 15:20:44 +0530

    list added

---
+++ list.py
@@ -0,0 +1,23 @@
+"""
+l1 = [1,2,34,4];
+print(type(l1));
+#<class 'list'>
+"""
+"""
+l2 =[40,True,4.5,3+4j,10,'mysirg'];
+print(l2);
+#[40, True, 4.5, (3+4j), 10, 'mysirg']
+"""
+
+"""
+l2 =[40,True,4.5,3+4j,10,'mysirg'];
+for var in l2:
+    print(var,end=" ");
+"""
+
+l1=[];
+l1.append(70);
+#print(l1);
+#l1.insert(2,23);
+l1.insert(23);
+print(l1);
