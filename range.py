a21c8b530eba895b8c8923e0e1f96f8d4f8b1bdb
Shivanibhadoriya <shivanibhadouriya2003@gmail.com>
2024-07-25 14:50:03 +0530

    Initial commit

---
+++ range.py
@@ -0,0 +1,18 @@
+"""
+x = range(1,9,2);
+for var in x:
+   print(var,end=" ");
+"""
+
+#print first n natural number
+"""
+n= int(input("enter the value of n "));
+x = range(1,n+1,1);
+for i in x:
+    print(i,end=" ");
+"""
+#print first square of first n natural number
+
+n = int(input("enter value of n"));
+for var in range(1,n+1):
+    print(var**2,end=" ");
